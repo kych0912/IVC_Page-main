b08e1ad8e608f37cfcedfd2124df43782d2fc8f7
YoungCheon Kim <kyt031522@gmail.com>
2024-03-20 21:15:38 +0900

    .

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 app.use(
     '/api',
         createProxyMiddleware({
-            target: 'http://3.91.196.73:5000',
+            target: 'http://ec2-3-91-196-73.compute-1.amazonaws.com:5000',
             changeOrigin: true,
         })
     );
